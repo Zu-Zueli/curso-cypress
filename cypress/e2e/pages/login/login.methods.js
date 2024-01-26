83216ee174baf064d9eabb45e49f96f7dd7f3c94
Zu-Zueli <zooeli@gmail.com>
2024-01-26 13:45:35 +0100

    Login Page

---
+++ cypress/e2e/pages/login/login.methods.js
@@ -0,0 +1,23 @@
+import { LoginElements } from "./login.elements";
+
+//Clase LoginMethods: para insertar los métodos posibles en la pagina de login
+export class LoginMethods {
+    //Método para insertar un nombre de usuario
+     static insertUsername(username){
+        LoginElements.input.username.type(username)
+     }
+     //Método para insertar una contraseña de usuario
+     static insertPassword(password){
+        LoginElements.input.password.type(password)
+     }
+     //Método para hacer click en el botón de login
+     static clickOnLoginButton(){
+        LoginElements.buttons.login.click()
+     }
+     //Metodo completo para loguearse
+     static login(username, password){
+        this.insertUsername(username)
+        this.insertPassword(password)
+        this.clickOnLoginButton()
+     }
+}
