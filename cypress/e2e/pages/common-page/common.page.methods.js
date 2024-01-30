3b8c1141d6982837b95b5e90b858687cc408f714
Zu-Zueli <zooeli@gmail.com>
2024-01-30 18:38:38 +0100

    Creacion de pagina Common page

---
+++ cypress/e2e/pages/common-page/common.page.methods.js
@@ -0,0 +1,27 @@
+import { CommonPageData } from "./common.page.data";
+import { CommonPageElements } from "./common.page.elements";
+
+export class CommonPageMethods{
+    static navigateToDemoBlaze(){
+        cy.visit(CommonPageData.url);
+    }
+
+    static clickOnHomeOption(){
+        CommonPageElements.menu.home.click()
+    }
+    static clickOnContactOption(){
+        CommonPageElements.menu.contact.click()
+    }
+    static clickOnAboutUsOption(){
+        CommonPageElements.menu.aboutUs.click()
+    }
+    static clickOnCartOption(){
+        CommonPageElements.menu.cart.click()
+    }
+    static clickOnLogInOption(){
+        CommonPageElements.menu.logIn.click()
+    }
+    static clickOnSignUpOption(){
+        CommonPageElements.menu.signUp.click()
+    }
+}
