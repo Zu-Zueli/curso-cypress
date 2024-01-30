bd39172854360cc1c547c5f596b1c382867dfe14
Zu-Zueli <zooeli@gmail.com>
2024-01-30 15:59:40 +0100

    Creacion de pagina de Cart

---
+++ cypress/e2e/pages/cart/cart.methods.js
@@ -0,0 +1,10 @@
+import { CartElements } from "./cart.elements";
+
+export class CartMethods{
+    static clickOnDeleteLink(productName){
+        CartElements.links.delete(productName).click()
+    }
+    static clickOnPaceOrderButton(){
+        CartElements.buttons.placeOrder.click()
+    }
+}
