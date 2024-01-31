cda275f54fc6f6f777f4dd393813c3d86d6b7ab1
Zu-Zueli <zooeli@gmail.com>
2024-01-31 15:27:22 +0100

    Eliminar video de Cypress

--- cypress/e2e/pages/common-page/common.page.elements.js
+++ cypress/e2e/pages/common-page/common.page.elements.js
@@ -3,19 +3,19 @@ export class CommonPageElements{
         return{
             get home(){
                 return cy.contains("a", "Home")
-            }
+            },
             get contact(){
                 return cy.contains("a", "Contact")
-            }
+            },
             get aboutUs(){
                 return cy.contains("a", "About us")
-            }
+            },
             get cart(){
                 return cy.contains("a", "Cart")
-            }
+            },
             get logIn(){
                 return cy.contains("a", "Log in")
-            }
+            },
             get signUp(){
                 return cy.contains("a", "Sign up")
             }
